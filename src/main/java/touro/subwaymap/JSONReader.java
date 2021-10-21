a260704a88644202e8d608298a75f62f02c85be6
Rmunitz-oss <58051290+Rmunitz-oss@users.noreply.github.com>
2021-10-20 21:25:59 -0400

    Fixed indentation

--- src/main/java/touro/subwaymap/JSONReader.java
+++ src/main/java/touro/subwaymap/JSONReader.java
@@ -1,7 +1,6 @@
 package touro.subwaymap;
 
 import com.google.gson.Gson;
-
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
@@ -11,10 +10,10 @@ public class JSONReader {
     public Stations stations;
 
     public Stations readJSON() throws IOException {
-            Gson gson = new Gson();
-            Reader reader = Files.newBufferedReader(Paths.get("src/main/resources/SubwayStations.json"));
-            stations = gson.fromJson(reader, Stations.class);
-            reader.close();
+        Gson gson = new Gson();
+        Reader reader = Files.newBufferedReader(Paths.get("src/main/resources/SubwayStations.json"));
+        stations = gson.fromJson(reader, Stations.class);
+        reader.close();
         return stations;
     }
 }
